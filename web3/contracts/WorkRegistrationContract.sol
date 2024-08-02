250f2fbd780b26e373a009545c86c91043338bec
james <jean.philippe@fedapay.com>
2024-08-02 15:33:17 +0100

    Instantiate User and Work contract

--- web3/contracts/WorkRegistrationContract.sol
+++ web3/contracts/WorkRegistrationContract.sol
@@ -18,6 +18,7 @@ enum WorkStatus{
     string description;
     uint amount;
     string createdAt;
+    string lastUpdatedAt;
     WorkStatus status ;
   }
 
@@ -27,9 +28,10 @@ enum WorkStatus{
     string memory title,
     string memory description,
     uint amount,
-    string memory createdAt
+    string memory createdAt,
+    string memory lastUpdatedAt
   )public {
-    idToWork[workCounter] = Work(workCounter,msg.sender, title, description, amount,createdAt, WorkStatus.PENDING);
+    idToWork[workCounter] = Work(workCounter,msg.sender, title, description, amount,createdAt, lastUpdatedAt, WorkStatus.PENDING);
     workCounter++;
   }
 
@@ -46,6 +48,42 @@ enum WorkStatus{
     (bool resp, ) = msg.sender.call{value: work.amount}("");
     
     return (resp, work.workId, work.owner, work.title, work.description, work.createdAt,work.status );
+  }
+  function getInternalWork(uint workId) public view returns (
+    uint _workId,
+    address _owner,
+    string memory title,
+    string memory description,
+    string memory createdAt,
+    WorkStatus status 
+  ) {
+
+    Work memory work= idToWork[workId];
+    return ( work.workId, work.owner, work.title, work.description, work.createdAt,work.status );
+
+  }
+
+function updateWork(
+    uint workId,
+    string memory title,
+    string memory description,
+    uint amount,
+    string memory lastUpdatedAt
+  )public {
+    require (idToWork[workId].amount!=0, "Work Art doesn't exist");
+    require(idToWork[workId].owner==msg.sender, "You can't edit this work art");
+    Work storage work = idToWork[workId];
+    work.title = title;
+    work.description = description;
+    work.amount= amount;
+    work.lastUpdatedAt = lastUpdatedAt;
+  }
+
+  function listWork() public returns (Work []memory ) {
+      address owner = msg.sender;
+      
+
+      for (uint i=0, )    
   }
 
 }
